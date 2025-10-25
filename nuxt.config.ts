acb253dbc36cb4f2389205fb6fc96117f43c0244
wuyong <biggodw@gmail.com>
2025-10-25 14:49:56 +0800

    edgeone 部署失败问题

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+   nitro: {
+    preset: 'node-server' // 或根据 EdgeOne 要求调整
+  }
 })
